5515617cdc33d131e6b063f02bbe5d3cfe8c58a6
Tyagachev <92306548+Tyagachev@users.noreply.github.com>
2022-06-02 12:35:09 +0300

    Add files via upload

---
+++ dateDiff/js/loadScript.js
@@ -0,0 +1,11 @@
+console.log ('loadScript');
+
+
+function loadScript(url, callback) {
+    const script = document.createElement('script')
+
+    script.src = url;
+    script.onload = callback;
+
+    document.body.appendChild(script)
+};
