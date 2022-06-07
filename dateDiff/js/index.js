60da0cae43685a3e5c6bba4f5d43993af02d295f
Anton <570uz@mail.ru>
2022-06-07 14:13:12 +0300

    Дз № 2

---
+++ dateDiff/js/index.js
@@ -0,0 +1,14 @@
+//import './form.js'
+import './main.js'
+import './loadScript.js'
+import './useFull.js'
+import './printResult.js'
+
+
+import photoMode from '../photomode.png' 
+
+import '../css/main.css'
+import '../style/main.scss'
+
+import xor  from "lodash/xor"
+console.log (xor([2,1], [2,3]))
